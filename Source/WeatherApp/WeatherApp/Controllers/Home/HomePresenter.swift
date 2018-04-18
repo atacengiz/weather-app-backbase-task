2ab8101bf4632d0403f673e9461123d1eb2f4fc7
Ata Cengiz <mygamezoneapp@gmail.com>
2018-04-18 14:59:41 +0200

    Added store, show and remove city information with persistance

--- Source/WeatherApp/WeatherApp/Controllers/Home/HomePresenter.swift
+++ Source/WeatherApp/WeatherApp/Controllers/Home/HomePresenter.swift
@@ -9,7 +9,29 @@
 import Foundation
 
 protocol HomePresenter {
+	func showStoredCities()
+	func removeCity(cityInfo: CityWeatherInformation)
 }
 
 class HomePresenterImplementation: HomePresenter {
+	
+	// MARK: Public variables
+	
+	// MARK: Private variables
+	private let cityWeatherPersistanceHelper: CityWeatherPersistanceHelper
+	private weak var viewController: HomeViewController?
+
+	init(cityWeatherPersistanceHelper: CityWeatherPersistanceHelper, viewController: HomeViewController) {
+		self.cityWeatherPersistanceHelper = cityWeatherPersistanceHelper
+		self.viewController = viewController
+	}
+	
+	func showStoredCities() {
+		guard let storedCities = cityWeatherPersistanceHelper.getStoredCityInformation() else { return }
+		viewController?.updateStoredCities(cities: storedCities)
+	}
+	
+	func removeCity(cityInfo: CityWeatherInformation) {
+		cityWeatherPersistanceHelper.removeCityInformation(info: cityInfo)
+	}
 }
---
+++ Source/WeatherApp/WeatherApp/Models/Coordinates.swift
@@ -0,0 +1,14 @@
+//
+//  Coordinates.swift
+//  WeatherApp
+//
+//  Created by Ata Cenqiz on 18/04/2018.
+//  Copyright © 2018 Mobiquity Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Coordinates: Codable {
+	var lon: Double
+	var lat: Double
+}
